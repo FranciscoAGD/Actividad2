587ae46900d2654e15ad8d3fd2538b8ba3ed142d
Daniel Castillejos <al07099742@tecmilenio.mx>
2026-03-23 16:57:22 -0700

    merge division

--- calculadora.py
+++ calculadora.py
@@ -1,33 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-def multuplicacion(a,b):
-    return a * b
-
-if __name__ == '__main__':
-    resultado_mult = multuplicacion(8,2)
-    print("El resultado de la multipliacion es:" ,resultado_mult)
-
-def suma ():
+def suma():
     resultado = 8 + 2
     print("El resultado de la suma de 8 + 2 es:", resultado)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     suma()
-=======
+
 def restar(a, b):
     return a - b
 
 if __name__ == "__main__":
     resultado = restar(15, 6)
     print("El resultado de la resta es:", resultado)
->>>>>>> origin/resta
-=======
+
 def dividir(a, b):
     if b == 0:
-        return "Error: division entre cero"
+        return "Error: división entre cero"
     return a / b
 
 if __name__ == "__main__":
     resultado = dividir(15, 3)
-    print("El resultado de la division es:", resultado)
->>>>>>> origin/division
+    print("El resultado de la división es:", resultado)
+
