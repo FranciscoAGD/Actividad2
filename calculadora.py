590a86cbea97a38ea4b94266ea766b40bc302a57
francisco <chulopro_8@outlook.com>
2026-03-23 12:06:59 -0700

    Creacion de funcion mult y cambios

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,7 @@
-def multuplicacion():
-    resultado_mult = 8 * 2
-    print("El resultado de la multipliacion de 8 * 2 es:", resultado_mult)
+def multuplicacion(a,b):
+    return a * b
+
+if __name__ == '__main__':
+    resultado_mult = multuplicacion(8,2)
+    print("El resultado de la multipliacion es:" ,resultado_mult)
 
