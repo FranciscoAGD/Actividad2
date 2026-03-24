e70898fec577f0d1cff0bcaf522926455c2e5438
Daniel Castillejos <al07099742@tecmilenio.mx>
2026-03-23 17:00:36 -0700

    Cambios de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -4,10 +4,3 @@ def multuplicacion(a,b):
 if __name__ == '__main__':
     resultado_mult = multuplicacion(8,2)
     print("El resultado de la multipliacion es:" ,resultado_mult)
-
-def suma ():
-    resultado = 8 + 2
-    print("El resultado de la suma de 8 + 2 es:", resultado)
-
-if __name__ == '__main__':
-    suma()
